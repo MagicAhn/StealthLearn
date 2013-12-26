56149be1f33894245d2de9465fe211f5ed57e94a
Ahn <rogerowen@qq.com>
2013-12-27 00:34:05 +0800

    Some Change

---
+++ StealthLearn/Assets/Done/DoneScripts/UnappliedScripts/guiTextSet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guiTextSet : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        guiText.text = "\nArrow Keys: Move\nLeft Shift: Sneak\nZ: Use Switch\nX: Attract Attention";
+
+    }
+}
