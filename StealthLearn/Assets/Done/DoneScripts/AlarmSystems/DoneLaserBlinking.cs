4ceafbfbfcee457986529032d9f73ff50b4d6c59
MagicAhn <rogerowen@qq.com>
2013-12-30 14:49:40 +0800

    all over

---
+++ StealthLearn/Assets/Done/DoneScripts/AlarmSystems/DoneLaserBlinking.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DoneLaserBlinking : MonoBehaviour
+{
+		public Single onTime;
+		public Single offTime;
+
+		private Single timer;
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				timer += Time.deltaTime;
+
+				if (renderer.enabled && timer > onTime) {
+						SwitchBeam ();
+				}
+				if (!renderer.enabled && timer > offTime) {
+						SwitchBeam ();
+				}
+		}
+
+		void SwitchBeam ()
+		{
+				// 重置 timer
+				timer = 0f;
+
+				renderer.enabled = !renderer.enabled;
+				light.enabled = !light.enabled;
+		}
+}
